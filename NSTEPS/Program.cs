2b3f421623291edcef4901aab361e1a2d323979b
mateusz7812 <mateusz78122@gmail.com>
2019-06-06 17:57:09 +0200

    Dodaj pliki projektów.

---
+++ NSTEPS/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace NSTEPS
+{
+    public class Program
+    {
+        static void Main()
+        {
+            long pointsAmount = Convert.ToInt32(Console.ReadLine());
+            for(long i = 0;i<pointsAmount; i++)
+            {
+                var Point = Console.ReadLine().Split().Select(s => Convert.ToInt64(s)).ToArray();
+                if (Point[0] == Point[1])
+                {
+                    Console.WriteLine(GetValue(new long[] { 0, 0 }, 0, 1, Point));
+                }
+                else if (Point[0] - 2 == Point[1])
+                    Console.WriteLine(GetValue(new long[] { 2, 0 }, 2, 1, Point));
+                else
+                    Console.Write("No Number\n");
+            }
+        }
+
+        public static long GetValue(long[] actual, long number, long step,long[] point)
+        {
+            if (actual[0] == point[0] & actual[1] == point[1])
+                return number;
+            number += step;
+            if (step == 1)
+                step = 3;
+            else
+                step = 1;
+            actual[0] += 1;
+            actual[1] += 1;
+
+            return GetValue(actual, number, step, point);
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSTEPS;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int result = Program.GetValue(new int[] { 0, 0 }, 0, 1, new int[] { 4, 4 });
+            Assert.AreEqual(8, result);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            int result = Program.GetValue(new int[] { 2, 0 }, 2, 1, new int[] { 4, 2 });
+            Assert.AreEqual(6, result);
+        }
+    }
+}
